4059d7879453fe631e576aa08f3f0c47d3d16dfe
Bruno Faria <bruno.fariadevl@gmail.com>
2022-08-12 15:39:23 -0300

    v1.2 - Atualização no código - retirada lista de vértices. Not used

--- Matriz Adjacencia e Incidencia/matriz-nao-direcionado.py
+++ Matriz Adjacencia e Incidencia/matriz-nao-direcionado.py
@@ -1,10 +1,11 @@
 import numpy as np
 
 class Grafo:
-    def __init__(self):
-        self.vertices = []
+    def __init__(self, qnt_vertices):
+        self.vertices = qnt_vertices # Substituir por lista se precisar dos vértices
         self.arestas = []
 
+    # Adicionar vértice se precisar da lista
     def add_vertice(self, qnt_vertices):
         for i in range(0, qnt_vertices):
             self.vertices.append(i + 1)
@@ -13,14 +14,14 @@ class Grafo:
         self.arestas.append([A, B])
     
     def gerar_matrizI(self):
-        matrizInc = np.zeros((len(self.vertices), len(self.arestas)))
+        matrizInc = np.zeros((self.vertices, len(self.arestas)))
         for j in range(0, len(self.arestas)):
             matrizInc[self.arestas[j][0] - 1][j] = 1
             matrizInc[self.arestas[j][1] - 1][j] = 1
         return matrizInc
 
     def gerar_matrizA(self):
-        matrizAdj = np.zeros((len(self.vertices), len(self.vertices)))
+        matrizAdj = np.zeros((self.vertices, self.vertices))
         for j in range(0, len(self.arestas)):
             matrizAdj[self.arestas[j][0] - 1][self.arestas[j][1] - 1] = 1
             matrizAdj[self.arestas[j][1] - 1][self.arestas[j][0] - 1] = 1 # Não direcionado -> tem relação X -> Y e Y -> X
