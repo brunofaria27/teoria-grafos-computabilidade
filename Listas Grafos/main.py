01ff986c9eebc47e5951f915b22babd5bec8ece6
Bruno Rodrigues Faria <bruno.fariadevl@gmail.com>
2022-08-18 18:17:46 -0300

    Listas Grafos - Forward e Reverse Star (OK) - Falta terminar os returns para user

--- Listas Grafos/main.py
+++ Listas Grafos/main.py
@@ -2,23 +2,9 @@ class Grafo:
     def __init__(self, numVertices: int, numArestas: int, lista_graph: list):
         self.numVertices = numVertices
         self.numArestas = numArestas
-        self.origem = self.create_origem(lista_graph)
-        self.destino = self.create_destino(lista_graph)
-        self.relacoes = self.create_relacao(lista_graph)
-    
-    def create_origem(self, lista_graph: list) -> list:
-        origem = []
-        for i in range(2, len(lista_graph)):
-            if i % 2 == 0:
-                origem.append(lista_graph[i])
-        return origem
-
-    def create_destino(self, lista_graph: list):
-        destino = []
-        for i in range(2, len(lista_graph)):
-            if i % 2 != 0:
-                destino.append(lista_graph[i])
-        return destino
+        self.relacoes = self.sorting(self.create_relacao(lista_graph))
+        self.origem = [x[0] for x in self.relacoes]
+        self.destino = [x[1] for x in self.relacoes]
     
     def create_relacao(self, lista_graph: list) -> list:
         relacoes = []
@@ -27,17 +13,58 @@ class Grafo:
                 relacoes.append([lista_graph[i], lista_graph[i + 1]])
         return relacoes
     
-    def forward_star(self):
-        print('ORIGEM -> ' + str(self.origem))
-        print('DESTINO -> ' + str(self.destino))
+    def sorting(self, relacoes: list, situacao: int=0) -> list:
+        if situacao == 1:
+            relacao_ordenada = sorted(relacoes, key=lambda item: item[1])
+            return relacao_ordenada
+        relacao_ordenada = sorted(relacoes, key=lambda item: item[0])
+        return relacao_ordenada
 
-    def reverse_star(self):
-        print('ORIGEM -> ' + str(self.origem))
-        print('DESTINO -> ' + str(self.destino))
+    def forward_star(self) -> list:
+        # Ordenar e criar array de destino e origem -> forward_star
+        new_relacao = self.sorting(self.relacoes, 1) # Ordena a relação pelo origem
+        origem = [x[0] for x in new_relacao] # Cria o array de origem
+        pointer = []
+        for i in range(0, len(origem)):
+            if i == 0:
+                pointer.append(i)
+            else:
+                if origem[i] != origem[i-1]:
+                    pointer.append(i)
+        pointer.append(pointer[len(pointer) - 1] + 1)
+        return pointer
 
-    def print_destin_orig(self):
-        print('ORIGEM -> ' + str(self.origem))
-        print('DESTINO -> ' + str(self.destino))
+    def reverse_star(self) -> list:
+        # Ordenar e criar array de destino e origem -> reverse_star
+        new_relacao = self.sorting(self.relacoes, 1) # Ordena a relação pelo destino
+        destino = [x[1] for x in new_relacao] # Cria o array de destino
+        pointer = []
+        pointer.append(0)
+        for i in range(0, len(destino)):
+            if i == 0:
+                pointer.append(i)
+            else:
+                if destino[i] != destino[i-1]:
+                    pointer.append(i)
+        pointer.append(pointer[len(pointer) - 1] + 1)
+        return pointer
+
+        # TODO: Receber um vértice e mostrar o grau de saída do mesmo
+        def get_grau_saida(self, vertice: int) -> int:
+            print('Grau de saida')
+        
+        # TODO: Receber um vértice e mostrar o grau de entrada do mesmo
+        def get_grau_entrada(self, vertice: int) -> int:
+            print('Grau entrada')
+
+        # TODO: Receber um vértice e mostrar o conjunto de sucessores do mesmo
+        def get_conj_sucessores(self, vertice: int) -> list:
+            print('Conjunto sucessores')
+        
+        # TODO: Receber um vértice e mostrar o conjunto de predecessores do  mesmo
+        def get_conj_predecessores(self, vertice: int) -> list:
+            print('Conjunto predecessores')
+    
 
 def get_graph(arquivo : str) -> list:
     arq = open(arquivo, 'r')
@@ -52,12 +79,10 @@ def get_graph(arquivo : str) -> list:
     return lista_graph
 
 def main():
-    dir_name = input()
+    dir_name = input('Digite o nome do arquivo (se estiver no mesmo dir) ou coloque o diretorio com o nome do arquivo: ')
     lista_graph = get_graph(dir_name)
     grafo = Grafo(lista_graph[0], lista_graph[1], lista_graph)
-    #
-    grafo.forward_star()
-
+    vertice_pesquisa = input('Qual o vértice que irá ser pesquisado? ')
 
 if __name__ == "__main__":
     main()
