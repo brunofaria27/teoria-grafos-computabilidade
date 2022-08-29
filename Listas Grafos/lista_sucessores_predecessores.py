06f19e378fe0f70a2e9e6ec58f49faff9a657f1b
Bruno Rodrigues Faria <bruno.fariadevl@gmail.com>
2022-08-29 18:59:05 -0300

    Add resume 8 and most performace in lista_sucessores_predecessores.py

--- Listas Grafos/lista_sucessores_predecessores.py
+++ Listas Grafos/lista_sucessores_predecessores.py
@@ -1,40 +1,38 @@
 import time
 import sys
+import re
 
 class Grafo:
-    def __init__(self, numVertices: int, numArestas: int, lista_graph: list):
+    def __init__(self, numVertices: int, numArestas: int):
         self.numVertices = numVertices
         self.numArestas = numArestas
-        self.relacao = self.sorting(self.create_relacao(lista_graph))
+        self.relacao = list()
 
-    def create_relacao(self, lista_graph: list) -> list:
-        relacoes = []
-        for i in range(2, len(lista_graph)):
-            if i % 2 == 0:
-                relacoes.append([lista_graph[i], lista_graph[i + 1]])
-        return relacoes
+    def addEdges(self, origin: int, dest: int):
+        self.relacao.append([int(origin), int(dest)])
 
-    def sorting(self, relacoes: list) -> list:
-        relacao_ordenada = sorted(relacoes, key=lambda item: item[0]) # Origem
+    def sorting(self, lis_relacao: list) -> list:
+        relacao_ordenada = sorted(lis_relacao, key=lambda item: item[0]) # Origem
         return relacao_ordenada
     
-    def getSucessor(self, vertex: int) -> list:
+    def getSucessor(self, vertex: int, lis_relacao: list) -> list:
         list_sucessor = []
-        for i in range(0, len(self.relacao)):
-            if self.relacao[i][0] == vertex:
-                list_sucessor.append(self.relacao[i][1])
+        for i in range(0, len(lis_relacao)):
+            if lis_relacao[i][0] == vertex:
+                list_sucessor.append(lis_relacao[i][1])
         return list_sucessor
 
-    def getPredecessor(self, vertex: int) -> list:
+    def getPredecessor(self, vertex: int, lis_relacao: list) -> list:
         list_predecessor = []
-        for i in range(0, len(self.relacao)):
-            if self.relacao[i][1] == vertex:
-                list_predecessor.append(self.relacao[i][0])
+        for i in range(0, len(lis_relacao)):
+            if lis_relacao[i][1] == vertex:
+                list_predecessor.append(lis_relacao[i][0])
         return list_predecessor
     
     def get_atributes(self, vertex: int) -> None:
-        lista_su = self.getSucessor(vertex)
-        lista_pre = self.getPredecessor(vertex)
+        lis_relacao = self.sorting(self.relacao)
+        lista_su = self.getSucessor(vertex, lis_relacao)
+        lista_pre = self.getPredecessor(vertex, lis_relacao)
         print("Lista de sucessores: " + str(lista_su))
         print("Lista de predecessores: " + str(lista_pre))
         print("Grau da saída: " + str(len(lista_su)))
@@ -42,15 +40,9 @@ class Grafo:
 
 def get_graph(arquivo : str) -> list:
     arq = open(arquivo, 'r')
-    lista_graph = []
-    for linha in arq:
-        linha = linha.rstrip('\n')
-        for i in linha.split(' '):
-            try:
-                lista_graph.append(int(i))
-            except ValueError:
-                continue
-    return lista_graph
+    content = arq.readlines()
+    content = [re.sub(' +', ' ', line.strip()) for line in content]
+    return content
 
 def main():
     start = time.time() # Get start time
@@ -59,7 +51,13 @@ def main():
     vertice_pesquisa = sys.argv[2] # Get vertice_pesquisa for command line
 
     lista_graph = get_graph(dir_name)
-    grafo = Grafo(lista_graph[0], lista_graph[1], lista_graph)
+    dados_aresta_vertice = list(lista_graph[0].split(' '))
+    grafo = Grafo(dados_aresta_vertice[0], dados_aresta_vertice[1])
+
+    for i in range(1, len(lista_graph)):
+        origem_destino = list(lista_graph[i].split(' '))
+        grafo.addEdges(origem_destino[0], origem_destino[1])
+
     grafo.get_atributes(int(vertice_pesquisa))
     
     end = time.time() # Finally get end of the program
